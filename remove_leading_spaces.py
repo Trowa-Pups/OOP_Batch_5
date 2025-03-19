6e67e9f50b587f725cdf2eca7d7d824e668eca03
unknown <sainttrowagmail.com>
2025-03-19 14:28:45 +0800

    Comment added

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -2,4 +2,4 @@
 name_input = str(input("Please input your name with several spaces at the beginning: "))
 
 #Print the input without the spaces in the beginning
-print(name_input.strip())
+print(name_input.strip()) #Using strip to remove the spaces
