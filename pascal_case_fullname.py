0a74c49024b798dbf38dc329789490d0f67d407f
unknown <sainttrowagmail.com>
2025-03-19 18:29:46 +0800

    Print the input in pascal casing

--- pascal_case_fullname.py
+++ pascal_case_fullname.py
@@ -2,3 +2,5 @@
 name_input = str(input("Please input your fullname in incorrect casing (Ex: TrOWa tANgcO): "))
 
 #Print the input in pascal casing
+name_input = name_input.title()
+print(name_input.replace(" ", ""))
