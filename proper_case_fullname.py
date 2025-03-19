0b4bfa9c3e4a74fd5e6f19d4eb0daad9ad9b8010
unknown <sainttrowagmail.com>
2025-03-19 16:09:30 +0800

    Print the input in proper casing

--- proper_case_fullname.py
+++ proper_case_fullname.py
@@ -2,3 +2,4 @@
 name_input = str(input("Please input your fullname in incorrect casing (Ex: TrOWa tANgcO): "))
 
 #Print the input in proper casing
+print(name_input.title()) #Use .title() to make it all first letters capitalized and the rest lowercase
