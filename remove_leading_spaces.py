bfc2ab8c35e879bb5e8454443893ad9c02d4101a
unknown <sainttrowagmail.com>
2025-03-19 14:19:10 +0800

    Ask the user to input their fullname with several space characters at the beginning

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,2 +1,3 @@
 #Ask the user to input their fullname with several space characters at the beginning
+name_input = str(input("Please input your name with several spaces at the begginning: "))
 #Print the input without the spaces in the beginning
