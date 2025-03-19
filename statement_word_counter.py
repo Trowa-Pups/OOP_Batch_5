c3d1cc9171b021be71a17b71fd8401ce708429cb
unknown <sainttrowagmail.com>
2025-03-19 17:23:49 +0800

    Count how many words are in the statement

--- statement_word_counter.py
+++ statement_word_counter.py
@@ -2,5 +2,6 @@
 statement_input = str(input("Please input a complete statement: "))
 
 #Count how many words are in the statement
+word_counter = len(statement_input.split())
 
 #Print the number of words
