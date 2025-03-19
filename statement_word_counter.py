60e1f9800d727dffe3ff0f668db58ba915e9b2eb
unknown <sainttrowagmail.com>
2025-03-19 17:30:52 +0800

    comment fixed

--- statement_word_counter.py
+++ statement_word_counter.py
@@ -2,7 +2,7 @@
 statement_input = str(input("Please input a complete statement: "))
 
 #Count how many words are in the statement
-word_counter = len(statement_input.split()) #Use len to count how many words
+word_counter = len(statement_input.split()) #Use len.split() to count how many words
 
 #Print the number of words
 print("Word count: ", word_counter)
