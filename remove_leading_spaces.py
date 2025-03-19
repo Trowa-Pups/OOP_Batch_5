aec6586ca6ed082afbf74d78bef0ea355fe52d94
unknown <sainttrowagmail.com>
2025-03-19 14:21:51 +0800

    Print the input without the spaces in the beginning, and fixed spelling at line 2

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,3 +1,5 @@
 #Ask the user to input their fullname with several space characters at the beginning
-name_input = str(input("Please input your name with several spaces at the begginning: "))
+name_input = str(input("Please input your name with several spaces at the beginning: "))
+
 #Print the input without the spaces in the beginning
+print(name_input.strip())
