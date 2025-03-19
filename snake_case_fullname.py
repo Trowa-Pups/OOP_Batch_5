c3914afdfdbff93ce2e9d05ad6acffd94a320fe9
unknown <sainttrowagmail.com>
2025-03-19 18:41:26 +0800

    Print the input in snake casing

--- snake_case_fullname.py
+++ snake_case_fullname.py
@@ -2,3 +2,5 @@
 name_input = str(input("Please input your fullname in incorrect casing (Ex: TrOWa tANgcO): "))
 
 #Print the input in snake casing
+name_input = name_input.lower()
+print(name_input.replace(" ", "_"))
