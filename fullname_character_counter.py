bafd88d599c8caeb8fd4fc3f1cf2846d6ecb14c2
unknown <sainttrowagmail.com>
2025-03-19 18:05:24 +0800

    Print the number of character

--- fullname_character_counter.py
+++ fullname_character_counter.py
@@ -1,7 +1,8 @@
 #Ask the user to input their fullname
-name_input = str(input("Please input your full name:"))
+name_input = str(input("Please input your full name: "))
 
 #Count how many characters 
 character_counter = len(name_input)
 
 #Print the number of characters
+print("Character count: ", character_counter)
