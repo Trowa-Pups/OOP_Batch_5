adb16279682507f93ab975eefe1ccbb9e067a48c
unknown <sainttrowagmail.com>
2025-03-19 14:41:04 +0800

    Ask the user to input a number from 0 - 1000

--- zero_fill_six_digit_format.py
+++ zero_fill_six_digit_format.py
@@ -1,2 +1,4 @@
 #Ask the user to input number from 0 - 1000
+number_input = int(input("Please input a number from 0 - 1000: "))
+
 #Print the number in six digit format and add zeros at the beginning to complete the format
