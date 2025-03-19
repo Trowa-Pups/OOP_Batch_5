07fba53a4d31b7e7dfb47605214e010b585f6f09
unknown <sainttrowagmail.com>
2025-03-19 17:22:28 +0800

    Ask the user to input a complete statement

--- statement_word_counter.py
+++ statement_word_counter.py
@@ -1,3 +1,6 @@
 #Ask the user to input a complete statement
+statement_input = str(input("Please input a complete statement: "))
+
 #Count how many words are in the statement
+
 #Print the number of words
