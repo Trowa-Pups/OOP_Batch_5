8ea38793a70459a3dec873512bf6dcdc7c14dcb1
unknown <sainttrowagmail.com>
2025-03-19 14:45:42 +0800

    Print the number in six digit format and add zeros at the beginning to complete the format

--- zero_fill_six_digit_format.py
+++ zero_fill_six_digit_format.py
@@ -2,3 +2,4 @@
 number_input = int(input("Please input a number from 0 - 1000: "))
 
 #Print the number in six digit format and add zeros at the beginning to complete the format
+print(str(number_input).zfill(6)) #Use str() to make the input into a string so that it can use .zfill(6) to add 0s
