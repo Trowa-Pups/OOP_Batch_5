baef5f246f1ad489d1ee69f5ee4702222e33316d
unknown <sainttrowagmail.com>
2025-03-19 15:39:11 +0800

    Print the input in all uppercase

--- convert_input_uppercase.py
+++ convert_input_uppercase.py
@@ -1,4 +1,5 @@
 #Ask the user to input their fullname
-name_input = str(input("Please input your fullname"))
+name_input = str(input("Please input your fullname: "))
 
 #Print the input in all uppercase
+print(name_input.upper())
